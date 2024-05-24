72ca38254676ccf6082d3282d0fd2dd26952ab3c
rooney <bch3454@gmail.com>
2024-05-24 22:16:10 +0900

    feat: fetch 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <main className={styles.main}>test</main>;
+const fetchData = async () => {
+  try {
+    const res = await fetch(
+      "https://prod-dev.cccv.to/v1/api/bizcard-actions/find?findType=USER_URL&value=rooney",
+      {
+        cache: "no-cache",
+      }
+    );
+
+    return res.json();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export default async function Home() {
+  const data = await fetchData();
+
+  return <main className={styles.main}>{data.id}</main>;
 }
